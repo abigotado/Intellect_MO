7b1bd1e43b849872b9c3769080b87883046ec70d
Abigotado <82471392+Abigotado@users.noreply.github.com>
2021-05-05 15:47:11 +0300

    Ready to add Container + ListView

--- lib/widgets/price_item/price_item.dart
+++ lib/widgets/price_item/price_item.dart
@@ -9,27 +9,29 @@ class PriceItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(8),
-      padding: EdgeInsets.all(8),
+      margin: EdgeInsets.only(left: 25, top: 15, right: 25, bottom: 15),
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.white,
+          border: Border.all(width: 1, color: Color.fromRGBO(0, 17, 51, 0.03))
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             margin: EdgeInsets.only(bottom: 8),
-            child: Text(value.name,
-                style: TextStyle(
-                    fontSize: 22,
-                    color: Colors.indigo,
-                    fontWeight: FontWeight.bold)),
+            child: Container(
+              margin: EdgeInsets.only(bottom: 14),
+              child: Text(value.name,
+                  style: TextStyle(
+                      fontSize: 14,
+                      color: Color(0xFF001133),
+                      fontWeight: FontWeight.w500)),
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("${value.price} руб./час",
-                  style: TextStyle(fontSize: 26, color: Colors.indigo)),
-            ],
-          ),
+          Text("${value.price} руб./час",
+              style: TextStyle(fontSize: 12, color: Color.fromRGBO(0, 17, 51, 0.6), fontWeight: FontWeight.w400)),
         ],
       ),
     );
