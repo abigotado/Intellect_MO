6a2a1ff9f71a812cd6a141a95283f96406e413a0
Abigotado <82471392+Abigotado@users.noreply.github.com>
2021-08-01 12:22:15 +0300

    creating elements of contacts page

--- lib/pages/contacts_page.dart
+++ lib/pages/contacts_page.dart
@@ -2,20 +2,6 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
-final String arrowLeft = 'assets/icons/arrowleft.svg';
-final String watches = 'assets/icons/timeicon.svg';
-final Widget svgArrowLeft = SvgPicture.asset(
-  arrowLeft,
-  width: 10,
-  height: 20,
-);
-final Widget svgWatches = SvgPicture.asset(
-  watches,
-  width: 26,
-  height: 26,
-);
 
 class ContactsPage extends StatelessWidget {
   const ContactsPage({Key key}) : super(key: key);
@@ -25,25 +11,75 @@ class ContactsPage extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Roboto'),
       home: Scaffold(
-        body: SafeArea(
-          child: Container(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Container(
+        extendBodyBehindAppBar: true,
+        body: Container(
+          color: Color(0xFFF8FAFF),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Center(
+                  child: Container(
                     margin: EdgeInsets.only(
-                        left: 25, top: 5, right: 25, bottom: 20),
+                        left: 120, top: 0, right: 120, bottom: 20),
+                    padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
                     clipBehavior: Clip.hardEdge,
-                    decoration:
-                        BoxDecoration(borderRadius: BorderRadius.circular(10)),
-                    child: Text('Как с нами связаться'),
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(84, 193, 251, 0.05),
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(71),
+                            bottomRight: Radius.circular(71))),
+                    child: Container(
+                      margin: EdgeInsets.only(top: 100),
+                      height: 90,
+                      width: 90,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(45)),
+                      clipBehavior: Clip.hardEdge,
+                    ),
                   ),
-                  Container(
-                      margin: EdgeInsets.only(
-                          left: 25, top: 25, right: 25, bottom: 20),
-                      child: Text('Наш адрес')),
-                ],
-              ),
+                ),
+                Center(
+                  child: Container(
+                    margin: EdgeInsets.only(left: 120, top: 17, right: 120),
+                    child: Column(
+                      children: [
+                        Center(
+                            child: Text('Интеллект МО',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500))),
+                        Center(
+                            child: Text('Южный лёд',
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    color: Color.fromRGBO(0, 17, 51, 0.5)))),
+                      ],
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Container(
+                        margin: EdgeInsets.only(
+                            left: 20, top: 25, right: 10, bottom: 15),
+                        decoration: BoxDecoration(
+                            color: Color.fromRGBO(70, 199, 253, 0.03),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Column(
+                          children: [Text('Телефон'), Text('+79636334242')],
+                        )),
+                    Container(
+                        margin: EdgeInsets.only(top: 25, right: 20, bottom: 15),
+                        decoration: BoxDecoration(
+                            color: Color.fromRGBO(70, 144, 253, 0.03),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Column(
+                          children: [Text('Телефон'), Text('+79636334242')],
+                        )),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
@@ -52,9 +88,9 @@ class ContactsPage extends StatelessWidget {
             "Контакты".toUpperCase(),
             style: TextStyle(color: Colors.black),
           ),
-          backgroundColor: Color(0xFFF8FAFF),
+          backgroundColor: Colors.transparent,
           centerTitle: true,
-          elevation: 0,
+          elevation: 0.0,
         ),
       ),
     );
